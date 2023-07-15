4a0fd84e905113bd06a49d990adc7a565d7b340b
lazos <lazos@lazos.me>
2023-07-15 17:47:43 +0200

    example host

--- example/evaluate.sh
+++ example/evaluate.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+HOST=${HOST:-http://localhost:8080}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-curl -sL -X POST -H "Content-Type: application/json" -H "Origin: bogus" -d @$SCRIPT_DIR/evaluate.json http://localhost:8080/evaluate
+start_time=$(date +%s%N)
+curl -L -X POST -H "Content-Type: application/json" -H "Origin: bogus" -d @$SCRIPT_DIR/evaluate.json $HOST/evaluate
+end_time=$(date +%s%N)
+duration=$((($end_time - $start_time) / 1000000))
+echo $duration ms
